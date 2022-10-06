a77802169f4afa9ca6800e563c4f8f832ff864b1
Charly <carlos@reliby.com>
2022-10-06 22:09:38 +0200

    Added code to "RaisePropertyChanged" on "SelectedItems". Added buton to test it.

--- Source/Demo/MainWindowViewModel.cs
+++ Source/Demo/MainWindowViewModel.cs
@@ -1,41 +1,60 @@
-﻿using System.Collections.Generic;
+﻿using BlackPearl.Controls.Contract;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Input;
 
-using BlackPearl.Controls.Contract;
-
-using Prism.Commands;
-using Prism.Mvvm;
-
 namespace BlackPearl.Controls.Demo
 {
-    public class MainWindowViewModel : BindableBase
+  public class MainWindowViewModel: BindableBase
+  {
+    private List<Person> FSelectedItems;
+    public MainWindowViewModel()
     {
-        public MainWindowViewModel()
-        {
-            Source = new List<Person>(PersonDataProvider.GetDummyData());
-            SelectedItems = new List<Person>() { Source.FirstOrDefault() };
-            SelectedItems2 = new List<Person>();
-            ShowSelectedItemCommand = new DelegateCommand<IList<Person>>(ShowSelectedItemCommandAction);
-            AdvanceLookUpContract = new AdvanceLookUpContract();
-        }
-        public List<Person> Source { get; set; }
-        public List<Person> SelectedItems { get; set; }
-        public List<Person> SelectedItems2 { get; set; }
-        public ICommand ShowSelectedItemCommand { get; set; }
-        public ILookUpContract AdvanceLookUpContract { get; }
-        private void ShowSelectedItemCommandAction(IList<Person> data)
-        {
-            var stringBuilder = new StringBuilder();
-
-            foreach (Person p in data)
-            {
-                stringBuilder.AppendLine(p.Name);
-            }
-
-            MessageBox.Show(stringBuilder.ToString(), "Selected items");
-        }
+      Source = new List<Person>(PersonDataProvider.GetDummyData());
+      FSelectedItems = new List<Person>() { Source.FirstOrDefault() };
+      SelectedItems2 = new List<Person>();
+      ShowSelectedItemCommand = new DelegateCommand<IList<Person>>(ShowSelectedItemCommandAction);
+      AdvanceLookUpContract = new AdvanceLookUpContract();
+      AddItemCommand = new DelegateCommand<IList<Person>>(AddItemCommandAction);
     }
+
+    public List<Person> Source { get; set; }
+    public List<Person> SelectedItems { get => FSelectedItems; set => SetSelectedItems(value); }
+    public List<Person> SelectedItems2 { get; set; }
+    public ICommand ShowSelectedItemCommand { get; set; }
+    public ILookUpContract AdvanceLookUpContract { get; }
+    public ICommand AddItemCommand { get; set; }
+
+    private void ShowSelectedItemCommandAction(IList<Person> data)
+    {
+      var stringBuilder = new StringBuilder();
+
+      foreach (Person p in data)
+      {
+        stringBuilder.AppendLine(p.Name);
+      }
+
+      MessageBox.Show(stringBuilder.ToString(), "Selected items");
+    }
+
+    private void AddItemCommandAction(IList<Person> data)
+    {
+      //data.Add(Source.FirstOrDefault());
+      //SelectedItems = (List<Person>)data;
+      List<Person> Pepe = new List<Person>() { Source.FirstOrDefault() };
+      SelectedItems = Pepe;
+    }
+
+    private void SetSelectedItems(List<Person> value)
+    {
+      FSelectedItems = value;
+      RaisePropertyChanged("SelectedItems");
+    }
+
+  }
 }
